84696b6bf5aee542b846ac3e64c350c19cb60c8c
xavier bollart <xavierbollart@xaviers-mbp.home>
2017-01-29 17:03:40 +0100

    rename all pictures change texte add plan tab

--- index.php
+++ index.php
@@ -68,8 +68,9 @@ if (!$errName && !$errEmail && !$errMessage) {
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#band">PRESENTATION</a></li>
-            <li><a href="#tour">VOLS</a></li>
+            <li><a href="#presentation">PRESENTATION</a></li>
+            <li><a href="#plane">L'AVION</a></li>
+            <li><a href="#flights">VOLS</a></li>
             <li><a href="#contact">CONTACT</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
             <li class="dropdown">
@@ -85,6 +86,7 @@ if (!$errName && !$errEmail && !$errMessage) {
       </div>
     </nav>
 
+<!--Part 1 Carousel-->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
@@ -93,68 +95,44 @@ if (!$errName && !$errEmail && !$errMessage) {
           <li data-target="#myCarousel" data-slide-to="2"></li>
           <li data-target="#myCarousel" data-slide-to="3"></li>
           <li data-target="#myCarousel" data-slide-to="4"></li>
-          <li data-target="#myCarousel" data-slide-to="5"></li>
-          <li data-target="#myCarousel" data-slide-to="6"></li>
         </ol>
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
           <div class="item active">
-            <img src="images/pic01_opt.jpg" alt="description pic 01" width="1200" height="700">
+            <img src="images/header/pic01_opt.jpg" alt="Falaise d'Etretat, Normandie" width="1200" height="700">
             <div class="carousel-caption">
-              <h3>title pic 01</h3>
-              <p>texte pic 01</p>
-            </div>
-          </div>
-
-          <div class="item">
-            <img src="images/pic02_opt.jpg" alt="description pic 02" width="1200" height="700">
-            <div class="carousel-caption">
-              <h3>title pic 02</h3>
-              <p> text pic 02</p>
+              <h3>Falaise d'Etretat, Normandie</h3>
             </div>
           </div>
         
-          <div class="item">
-            <img src="images/pic03_opt.jpg" alt="description pic 03" width="1200" height="700">
+           <div class="item">
+            <img src="images/header/pic04_opt.jpg" alt="Près du Havre, Normandie" width="1200" height="700">
             <div class="carousel-caption">
-              <h3>title pic 03</h3>
-              <p>text pic 03</p>
+              <h3>Près du Havre, Normandie</h3>
             </div>
           </div>
 
            <div class="item">
-            <img src="images/pic04_opt.jpg" alt="description pic 04" width="1200" height="700">
+            <img src="images/header/pic05_opt.jpg" alt="Les falaises de Normandie<" width="1200" height="700">
             <div class="carousel-caption">
-              <h3>title pic 04</h3>
-              <p>text pic 04</p>
+              <h3>Les falaises de Normandie</h3>
             </div>
           </div>
 
            <div class="item">
-            <img src="images/pic05_opt.jpg" alt="description pic 05" width="1200" height="700">
+            <img src="images/header/pic06_opt.jpg" alt="Coucher de soleil autour de Paris" width="1200" height="700">
             <div class="carousel-caption">
-              <h3>title pic 05</h3>
-              <p>text pic 05</p>
-            </div>
-          </div>
-
-           <div class="item">
-            <img src="images/pic06_opt.jpg" alt="description pic 06" width="1200" height="700">
-            <div class="carousel-caption">
-              <h3>title pic 06</h3>
-              <p>text pic 06</p>
+              <h3>Coucher de soleil autour de Paris</h3>
             </div>
           </div>
 
           <div class="item">
-            <img src="images/pic07_opt.jpg" alt="description pic 07" width="1200" height="700">
+            <img src="images/header/pic07_opt.jpg" alt="La côte normande" width="1200" height="700">
             <div class="carousel-caption">
-              <h3>title pic 07</h3>
-              <p>text pic 07</p>
+              <h3>La côte normande</h3>
             </div>
           </div>
-
         </div>
 
         <!-- Left and right controls -->
@@ -168,10 +146,9 @@ if (!$errName && !$errEmail && !$errMessage) {
         </a>
     </div>
 
-    <!-- Container (The Band Section) -->
-    <div id="band" class="container text-center">
-      <h3 align="left">Presentation</h3>
-      <p align="left"><em>Qui sommes nous?</em></p>
+    <!-- Part 2 Presentation-->
+    <div id="presentation" class="container text-center">
+      <h3>La Companie</h3>
       <p align="left">Après 10 ans d’aviation, Vincent lance Le Pilote Français. 
       Cette compagnie aérienne en cours de certi cation par la DGAC (Direction Générale de l’Aviation Civile) 
       et qui sera située sur l’aérodrome parisien du Plessis-Belleville vous permettra de découvrir 
@@ -180,112 +157,168 @@ if (!$errName && !$errEmail && !$errMessage) {
      
       <div class="row">
         <div class="col-sm-12">
-          <p class="text-center"><strong>The Pilot</strong></p><br>
+          <!--<p class="text-center"><strong>The Pilot</strong></p><br>-->
           <a href="#demo" data-toggle="collapse">
-            <img src="images/vincent_opt.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+            <img src="images/presentation/pic08_opt.jpg" class="img-circle person" alt="Dirigeant responsable et pilote" width="800" height="532">
           </a>
           <div id="demo" class="collapse">
             <p>Vincent Bollart</p>
-            <p>Main pilot en CEO of the company</p>
-            <!--p>Member since 1988</p-->
+            <p>Dirigeant responsable et pilote</p>
           </div>
         </div>
       </div>
-
-      <h3 align="left">Déroulement d’un vol :</h3>
-      <p align="left">• Chauffeur privé depuis votre hôtel jusqu’à l’aéroport</p>
-      <p align="left">• Accueil à l’aéroport par le pilote</p>
-      <p align="left">• Briefing : présentation de l’avion, du vol et des consignes de sécurité</p>
-      <p align="left">• Départ en vol</p>
-      <p align="left">• Après le vol : Boisson o erte avec des gourmandises parisiennesW</p>
-      <p align="left">• Retour à Paris avec un chau eur privé</p>
-      <br>
-
-      <h3 align="left">Remarques importantes :</h3>
-      <p align="left">- Le Pilote Français est une compagnie aérienne respectant la réglementation en vigueur.</p>
-      <p align="left">- Le prix comprend le transport aller/retour depuis votre hôtel parisien 
-      jusqu’à l’aéroport et une assurance transport public couvrant les passagers et leurs a aires personnelles).</p>
-      <p align="left">- Règlement : 50% à la réservation sur le site et 50% avant le vol à l’aéroport.</p>
-      <p align="left">- Certaines conditions météorologiques peuvent impliquer l’annulation d’un 
-      vol au der- nier moment. Le vol est reporté dans la mesure du possible sinon l’avance à 
-      la réservation est remboursée.</p>
-      <br>
-
     </div>
 
-    <!-- Container (TOUR Section) -->
-    <div id="tour" class="bg-1">
-      <div class="container">
-        <h3 class="text-center">Available flights</h3>
-        <p class="text-center">Weater as to be checked before any flight<br> Please use the form below to book a flight</p>
-        <!--ul class="list-group">
-          <li class="list-group-item">September <span class="label label-danger">Sold Out!</span></li>
-          <li class="list-group-item">October <span class="label label-danger">Sold Out!</span></li>
-          <li class="list-group-item">November <span class="badge">3</span></li>
-        </ul-->
-        
-        <div class="row text-center">
-          <div class="col-sm-6">
-            <div class="thumbnail">
-              <img src="images/Etretat.jpg" alt="Paris" width="600" height="450">
-              <p><strong>Etretat's Cliff</strong></p>
-              <p>450 euros</p>
-              <button class="btn" data-toggle="modal" data-target="#myModal">Book it </button>
-            </div>
-          </div>
-          <div class="col-sm-6">
-            <div class="thumbnail">
-              <img src="images/ground.jpg" alt="New York" width="600" height="450">
-              <p><strong>Somme Bay</strong></p>
-              <p>450 euros</p>
-              <button class="btn" data-toggle="modal" data-target="#myModal">Book it</button>
-            </div>
-          </div>
-          <!--div class="col-sm-4">
-            <div class="thumbnail">
-              <img src="sanfran.jpg" alt="San Francisco" width="400" height="300">
-              <p><strong>San Francisco</strong></p>
-              <p>Sunday 29 November 2015</p>
-              <button class="btn" data-toggle="modal" data-target="#myModal">Buy Tickets</button>
-            </div>
-          </div-->
+    <div id="plane" class="container text-center">
+      <h3>L'avion</h3>
+      <p class="text-left"> Model: Diamond DA42 twin star </p>
+      <div class="row ">
+        <div class="col-sm-6">
+          <img src="images/plane/pic23.jpg" alt="DA 42" width="450" height="350"> 
         </div>
+        <div class="col-sm-6">
+          <img src="images/plane/pic22.jpg" alt="DA 42" width="450" height="350"> 
+        </div>
+      </div>
+    </div>
+    <br>
+
+    <!-- Part 3 flights -->
+    <div id="flights" class="bg-1">
+
+      <div class="container">
+      
+        <h3 class="text-center">Déroulement d’un vol</h3>
+        <br>
+        <p class="text-left">•  Chauffeur privé depuis votre hôtel jusqu’à l’aéroport</p>
+        <p class="text-left">•  Accueil à l’aéroport par le pilote</p>
+        <p class="text-left">•  Briefing : présentation de l’avion, du vol et des consignes de sécurité</p>
+        <p class="text-left">•  Départ en vol</p>
+        <p class="text-left">•  Après le vol : Boisson offerte avec des gourmandises parisiennes</p>
+        <p class="text-left">•  Retour à Paris avec un chaffeur privé</p>
+        <br>
+      
+        <h3 class="text-center">Quatre destinations au départ de Paris:</h3>
+        <br>
+
+        <div class="row text-center">
+        <div class="col-sm-6">
+          <div class="thumbnail">
+            <li data-toggle="modal" data-target="#picModal">
+              <a href="#myGallery" data-slide-to="0">
+                <img src="images/flight1/pic09.jpg" alt="Vol Etretat" width="350" height="210">
+              </a>
+            </li>
+            <p class"text-left" ><strong>Etretat</strong></p>
+            <p class"text-left">Venez découvrir la Normandie et ses majestueuses falaises</p>
+            <p class"text-left">- Temps de vol A/R sans escale : 1h30</p>
+            <p class"text-left">- Prix par personne : 690 <span class="glyphicon glyphicon-euro"></span> (cf Règlement)</p>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="thumbnail">
+           <li data-toggle="modal" data-target="#picModal">
+              <a href="#myGallery" data-slide-to="0">
+                <img src="images/flight2/pic13.jpg" alt="Vol baie de Somme" width="350" height="210">
+              </a>
+            </li>
+            <p class"text-left" ><strong>Baie de Somme</strong></p>
+            <p class"text-left">Survolez cette réserve natrelle classée au club des plus belles baies du monde</p>
+            <p class"text-left">- Temps de vol A/R sans escale : 1h30</p>
+            <p class"text-left">- Prix par personne : 690 <span class="glyphicon glyphicon-euro"></span> (cf Règlement)</p>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="thumbnail">
+            <li data-toggle="modal" data-target="#picModal">
+              <a href="#myGallery" data-slide-to="0">
+                <img src="images/flight3/pic24.jpg" alt="Vol paris" width="350" height="210">
+              </a>
+            </li>
+            <p class"text-left" ><strong>Paris la nuit</strong></p>
+            <p class"text-left">Vivez un instant magique en observant la capitale parisienne toute illuminée</p>
+            <p class"text-left">- Temps de vol A/R sans escale : 1h</p>
+            <p class"text-left">- Prix par personne : 850 <span class="glyphicon glyphicon-euro"></span> (cf Règlement)</p>
+          </div>
+        </div>
+
+        <div class="col-sm-6">
+          <div class="thumbnail">
+            <li data-toggle="modal" data-target="#picModal">
+              <a href="#myGallery" data-slide-to="0">
+                <img src="images/flight4/pic25.jpg" alt="Vol Reims" width="350" height="210">
+              </a>
+            </li>
+            <p class"text-left" ><strong>Reims et Champagne</strong></p>
+            <p class"text-left">Visite d’un château et dégustation de champagne : boisson préférée des Français</p>
+            <p class"text-left">- Temps de vol A/R avec escale : 1h20</p>
+            <p class"text-left">- Prix par personne : 1290 <span class="glyphicon glyphicon-euro"></span> (cf Règlement)</p>
+          </div>
+        </div>
+
+        <h3 align="left">Remarques importantes :</h3>
+        <p align="left">- Le Pilote Français est une compagnie aérienne respectant la réglementation en vigueur.</p>
+        <p align="left">- Le prix comprend le transport aller/retour depuis votre hôtel parisien 
+        jusqu’à l’aéroport et une assurance transport public couvrant les passagers et leurs a aires personnelles).</p>
+        <p align="left">- Règlement : 50% à la réservation sur le site et 50% avant le vol à l’aéroport.</p>
+        <p align="left">- Certaines conditions météorologiques peuvent impliquer l’annulation d’un 
+        vol au der- nier moment. Le vol est reporté dans la mesure du possible sinon l’avance à 
+        la réservation est remboursée.</p>
+        <br>
       </div>
       
-      <!-- Modal -->
-      <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog">
-        
-          <!-- Modal content-->
+    <!-- Modal picture -->
+    <div class="modal fade" id="picModal">
+      <div class="modal-dialog">
           <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">×</button>
-              <h4><span class="glyphicon glyphicon-lock"></span> Tickets</h4>
-            </div>
-            <div class="modal-body">
-              <form role="form">
-                <div class="form-group">
-                  <label for="psw"><span class="glyphicon glyphicon-shopping-cart"></span> Tickets, $23 per person</label>
-                  <input type="number" class="form-control" id="psw" placeholder="How many?">
-                </div>
-                <div class="form-group">
-                  <label for="usrname"><span class="glyphicon glyphicon-user"></span> Send To</label>
-                  <input type="text" class="form-control" id="usrname" placeholder="Enter email">
-                </div>
-                  <button type="submit" class="btn btn-block">Pay
-                    <span class="glyphicon glyphicon-ok"></span>
-                  </button>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal">
-                <span class="glyphicon glyphicon-remove"></span> Cancel
-              </button>
-              <p>Need <a href="#">help?</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">×</button>
+                <h4><span class="glyphicon glyphicon-lock"></span> Etreta's Cliff</h4>
+              </div>
+                
+              <div class="modal-body">
+                  <div id="myGallery" class="carousel slide" data-ride="carousel">
+                  <div class="carousel-inner">
+                  <div class="item active"> <img src="images/ground.jpg" alt="item0">
+                  <div class="carousel-caption">
+                  <h3>Heading 3</h3>
+                  <p>Slide 0  description.</p>
+                  </div>
+                  </div>
+                  <div class="item"> <img src="images/Etretat.jpg" alt="item1">
+                  <div class="carousel-caption">
+                  <h3>Heading 3</h3>
+                  <p>Slide 1 description.</p>
+                  </div>
+                  </div>
+                  <div class="item"> <img src="images/vincent_opt.jpg" alt="item2">
+                  <div class="carousel-caption">
+                  <h3>Heading 3</h3>
+                  <p>Slide 2  description.</p>
+                  </div>
+                  </div>  
+                  
+                  <!--end carousel-inner--></div>
+                  <!--Begin Previous and Next buttons-->
+                  <a class="left carousel-control" href="#myGallery" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span></a> 
+                  <a class="right carousel-control" href="#myGallery" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span></a>
+                  <!--end carousel--></div>
+
+              <!--end modal-body--></div>
+              <div class="modal-footer">
+              <div class="pull-left">
+              
+              </div>
+              <button class="btn-sm close" type="button" data-dismiss="modal">Close</button>
+              </div><!--end modal-footer-->
+          </div><!--end modal-content-->
+      </div><!--end modal-dialoge-->
+    </div><!--end myModal-->
+
+
+    </div>
     </div>
 
     <!-- Container (Contact Section) -->
@@ -327,6 +360,6 @@ if (!$errName && !$errEmail && !$errMessage) {
         </form>
       </div>
       <br>
-  </div>
+    </div>
   </body>
 </html>
